97f93424409f05e04f1654fcd7f6ced550cc9b49
Leandro Feitoza <p22.leandro@webmotors.com.br>
2022-03-05 22:16:48 -0300

    Logica do controller MusicaController

---
+++ Controllers/MusicaController.cs
@@ -0,0 +1,40 @@
+﻿using SistemaFooFaMvc.Models;
+using SistemaFooFaMvc.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaFooFaMvc.Controllers
+{
+    public class MusicaController : Controller
+    {
+        // GET: Musica
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(MusicaModel musica)
+        {
+            try
+            {
+                MusicaRepository musicRepo = new MusicaRepository();
+
+                if (musicRepo.AddMusica(musica))
+                {
+                    ViewBag.Message = "Música adicionada com sucesso!";
+                }
+                    return View();
+
+            }
+            catch (SqlException ex)
+            {
+
+                return View();
+            }
+        }
+    }
+}
