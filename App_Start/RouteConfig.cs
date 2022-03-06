af1752f42b2b49b2bd9ef969995dcaef0ac24b3e
Leandro Feitoza <p22.leandro@webmotors.com.br>
2022-03-06 15:32:23 -0300

    Router configurado para rota principal

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace SistemaFooFaMvc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Album",
+                url: "albuns/index",
+                defaults: new { controller = "Album", action = "Index" }
             );
         }
     }
